e255eff84161b062d9b3af3fc48810849bed04ff
lx779940 <3122178991@qq.com>
2023-11-20 20:14:08 +0800

    ddas

---
+++ node/spider/base/1.js
@@ -0,0 +1,16 @@
+// console.log('hello');
+// console.log(Date);
+// console.log(Math);
+
+// setTimeout(()=>{
+//     console.log(1);
+// },1000)
+
+// console.log(setInterval);
+// console.log(requestAnimationFrame);
+
+
+// console.log(__dirname);//文件夹绝对路径
+// console.log(__filename);//文件绝对路径
+
+console.log(process);
