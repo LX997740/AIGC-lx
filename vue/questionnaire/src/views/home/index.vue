234a5c091428539fc6dccef9347b64d84b58629f
lx779940 <3122178991@qq.com>
2023-12-19 17:04:37 +0800

    demo

---
+++ vue/questionnaire/src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home_container">
+        <item-container parent-Component="home" />
+    </div>
+</template>
+
+<script>
+import ItemContainer from '@/components/ItemContainer.vue'
+export default {
+    components: {
+        ItemContainer
+    }
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ vue/questionnaire/src/views/item/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ItemContainer parent-Component="item" />
+    </div>
+</template>
+
+<script>
+import ItemContainer from '@/components/ItemContainer.vue'
+export default {
+    components: {
+        ItemContainer
+    }
+}
+</script>
+
+<style lang="less" scoped></style>
