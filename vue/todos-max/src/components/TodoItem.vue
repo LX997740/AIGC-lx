75ee81bc32a6ece4c80d5496ff104c494fd0a57f
lx779940 <3122178991@qq.com>
2023-12-28 21:28:45 +0800

    修改

--- vue/todos-max/src/components/TodoItem.vue
+++ vue/todos-max/src/components/TodoItem.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-    </div>
+  <li class="todo" :class="{ completed: todo.done }"></li>
+  <div class="view">
+    <label v-text="todo.title"></label>
+  </div>
 </template>
 
 <script setup>
-
+import { defineProps } from "vue";
+defineProps({
+  todo: Object,
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
