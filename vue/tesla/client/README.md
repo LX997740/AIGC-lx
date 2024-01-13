0cbb8ee538bc2ec9a7201d9ee1dec25e4425fea1
lx779940 <3122178991@qq.com>
2024-01-13 17:51:13 +0800

    tesla

---
+++ vue/tesla/client/README.md
@@ -0,0 +1,23 @@
+# 安排的自己的项目中
+
+## animate.css
+
+    带来复杂度动画效果
+    import 'animate.css/animate.min.css'
+    import 'animate.css'
+
+## 性能优化
+
+    - 按需加载UI组件 ，减少代码体积，加速首页显示
+    - 路由懒加载
+    - debounce
+    - 图片懒加载
+
+## 良好的架构
+
+- MVVM 和组件思想
+- 设计 store
+- 高度还原 UI
+
+- 选品
+- 写文章
---
+++ vue/tesla/client/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+import "animate.css/animate.min.css";
+import "animate.css";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/reset.css";
+
+const store = createPinia();
+const app = createApp(App);
+
+//注册全局组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.use(router);
+app.use(store);
+//优化 按需加载
+app.use(ElementPlus);
+
+app.mount("#app");
