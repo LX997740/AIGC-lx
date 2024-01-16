5175e7b15af11f97510f0c6a1ee8439841fb5b0e
lx779940 <3122178991@qq.com>
2024-01-16 17:13:07 +0800

    disposition @

--- MyItems/GoWhere/client/vite.config.js
+++ MyItems/GoWhere/client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
+import path from "path";
+function resolve(url) {
+  return path.join(__dirname, url);
+}
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+    },
+  },
   plugins: [vue()],
-})
+});
