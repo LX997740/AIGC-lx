4b4a1f6a66572dd2358754111018f7a8f4f11cd0
lx779940 <3122178991@qq.com>
2024-01-19 13:24:21 +0800

    feat newstore

--- MyItems/GoWhere/client/src/store/useHomePageStore.js
+++ MyItems/GoWhere/client/src/store/useHomePageStore.js
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
+//推荐页仓库
 export const useRecommendStore = defineStore("/home", () => {
   const getRecommendList = () => {
     return [
@@ -37,5 +38,80 @@ export const useRecommendStore = defineStore("/home", () => {
   const Recommend = ref(getRecommendList());
   return {
     Recommend,
+  };
+});
+//瀑布流仓库
+export const useCartStore = defineStore("/Cart", () => {
+  const getCartList = () => {
+    return [
+      {
+        id: 1,
+        title: "我的收藏",
+        img: "https://cdn.pixabay.com/photo/2017/08/06/12/06/people-2591874_640.jpg",
+        name: "行走地区",
+        area: "北海道",
+        count: 99,
+      },
+      {
+        id: 2,
+        title: "我的收藏",
+        img: "https://img95.699pic.com/photo/60010/9023.jpg_wh300.jpg",
+        name: "行走地区",
+        area: "南昌",
+        count: 999,
+      },
+      {
+        id: 3,
+        title: "我的收藏",
+        img: "https://p1-q.mafengwo.net/s8/M00/93/86/wKgBpVYoiOiABLExAAJI8ZtJQtI51.jpeg",
+        name: "行走地区",
+        area: "北京",
+        count: 999,
+      },
+      {
+        id: 4,
+        title: "我的收藏",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGzu2g5guCDgE_tgh5T8LCdOG7Ger6zMmlC7VUy6mNGIpDto7b00wh55esdpZtMCy5of4&usqp=CAU",
+        name: "行走地区",
+        area: "西安",
+        count: 999,
+      },
+      {
+        id: 5,
+        title: "我的收藏",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGkU_ad1yuV0E--ACv6mveizmncqh7aBXDIXcrxS3cog6eYkyUa4CFOTvp93-k4-04Dds&usqp=CAU",
+        name: "行走地区",
+        area: "东北",
+        count: 999,
+      },
+      {
+        id: 6,
+        title: "我的收藏",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmG13bsBxPLwZrGIZP-RDK2KuZm7q2nUmG5KNENNzL7lPrWcloft21i9fMM8mCxB7BruI&usqp=CAU",
+        name: "行走地区",
+        area: "北海道",
+        count: 999,
+      },
+      {
+        id: 7,
+        title: "我的收藏",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9gUJR-RODjYfjhlPHOpIgGLmq2hPnydN8Wcb5QuGzD2KT3g2F_K8M_I6A7ojqhEwfbAI&usqp=CAU",
+        name: "行走地区",
+        area: "北海道",
+        count: 999,
+      },
+      {
+        id: 8,
+        title: "我的收藏",
+        img: "https://cdn.pixabay.com/photo/2017/08/06/12/06/people-2591874_640.jpg",
+        name: "行走地区",
+        area: "北海道",
+        count: 999,
+      },
+    ];
+  };
+  const Carts = ref(getCartList());
+  return {
+    Carts,
   };
 });
