055e2e24a46e550ac1df14b88d2901de184e28ca
lx779940 <3122178991@qq.com>
2024-01-19 12:04:43 +0800

    first commit

---
+++ MyItems/GoWhere/client/src/components/HomePage/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-1/2">
+    <div class="">
+      <img src="" alt="" />
+      <span>北海道</span>
+    </div>
+    <span>我的推荐</span>
+    <div class="flex justify-between">
+      <span>行走地区</span>
+      <span>9999</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  item: Object,
+});
+const item = defineProps.item;
+console.log(item);
+</script>
+
+<style scoped></style>
