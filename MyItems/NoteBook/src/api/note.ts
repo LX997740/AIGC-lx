4bdefdeb20265a42182d0b96c34f1284d4bb3a7a
lx779940 <3122178991@qq.com>
2024-01-24 18:10:08 +0800

    feat: add axios

--- MyItems/NoteBook/src/api/note.ts
+++ MyItems/NoteBook/src/api/note.ts
@@ -1,4 +1,4 @@
 import axios from '.'
-export function getNotes(page: number, size: number) {
-  return axios.get(`/note.page/${page}/${size}`)
+export function getNotes<T>(page: number, size: number) {
+  return axios.get<T>(`/note.page/${page}/${size}`) as Promise<T>
 }
