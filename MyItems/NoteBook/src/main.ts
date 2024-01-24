cfc6b777b37dfd190a18d2c83b39c17712f28cb5
lx779940 <3122178991@qq.com>
2024-01-24 17:06:46 +0800

    chore: add vant add use vant

--- MyItems/NoteBook/src/main.ts
+++ MyItems/NoteBook/src/main.ts
@@ -1,13 +1,15 @@
 import './assets/main.css'
-
+import 'vant/lib/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import { Button } from 'vant'
 const app = createApp(App)
 
+app.use(Button)
 app.use(createPinia())
 app.use(router)
 
