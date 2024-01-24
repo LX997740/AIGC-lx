ad3ad2903f0a4fde7e2ece54fcb5e34ef4293e9e
lx779940 <3122178991@qq.com>
2024-01-24 16:15:00 +0800

    uniapp base

---
+++ uniapp/base/components/song/song.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="song">
+		
+	</view>
+</template>
+
+<script setup>
+defineProps({
+	data:Object
+})
+</script>
+
+<style>
+
+</style>
---
+++ uniapp/base/pages/about/about.vue
@@ -0,0 +1,12 @@
+<template>
+	<view>abouta</view>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+onMounted(() => {
+	
+});
+</script>
+
+<style lang="scss"></style>
