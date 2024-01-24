9f2f6b1e0d6dceb3cd609ecb492c2622beba581a
lx779940 <3122178991@qq.com>
2024-01-24 18:09:02 +0800

    style: add app.vue style

--- MyItems/NoteBook/src/App.vue
+++ MyItems/NoteBook/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <header> 
-    <van-button type="primary"> 点击</van-button>
-  </header>
+  <div class="box">
+    <router-view />
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
+<style scoped lang="scss">
+.box {
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-flow: column nowrap;
+}
 </style>
