79c4882618d26f2185af45db6aa891a93b5f559a
lx779940 <3122178991@qq.com>
2024-01-26 16:38:55 +0800

    feat 添加首页等四个页面

--- MyItems/shopping/pages/index/index.vue
+++ MyItems/shopping/pages/index/index.vue
@@ -1,52 +1,18 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="index">
+		<view class="f-active-color">
+			文字
+		</view>
+		<view class="icon-fangdajing iconfont">
+			出来了嘛
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup>
 
-		},
-		methods: {
-
-		}
-	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
