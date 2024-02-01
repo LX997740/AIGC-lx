aff4b7d1eba83576c6ffbaddcd0af405cc8be0ab
lx779940 <3122178991@qq.com>
2024-02-01 20:46:41 +0800

    feat :搜索接口的封装

---
+++ uniapp/music/api/search.js
@@ -0,0 +1,21 @@
+import baseUrl from './request.js'
+
+// 搜索页面的所有接口
+
+//热门搜索
+export const apiSearchHotDetail =()=> {
+	return uni.request({
+		url:`${baseUrl}/search/hot/detail`,
+		method: 'GET',
+	})
+}
+//搜索接口
+export const apiSearchSuggest =()=> {
+	return uni.request({
+		url:`${baseUrl}/search/suggest`,
+		method: 'GET',
+		data:{
+			keywords: keywords,
+		}
+	})
+}
