839b8c68b4b2b4ca99154f04797ae70f4fb91135
lx779940 <3122178991@qq.com>
2024-02-21 08:51:48 +0800

    feat: js实现vue响应式

---
+++ vue/test/nextTick/1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const str = ref("hello");
+console.log(str.value); //hello
+nextTick(() => {
+  str.value = "world";
+  console.log(str.value); // world
+});
+</script>
+
+<style lang="scss" scoped></style>
