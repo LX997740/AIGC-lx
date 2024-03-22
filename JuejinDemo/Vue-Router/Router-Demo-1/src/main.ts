6e13d99a97e06b262bab2f1e6184d1337140cb68
lx779940 <3122178991@qq.com>
2024-03-22 22:10:10 +0800

    Vue-router:demo-1

---
+++ JuejinDemo/Vue-Router/Router-Demo-1/src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+//导入路由
+import router from './router'
+
+const app = createApp(App)
+//挂载路由
+app.use(router)
+
+app.mount('#app')
---
+++ JuejinDemo/Vue-Router/Router-Demo-1/src/router/index.ts
@@ -0,0 +1,27 @@
+//用ES6解构导入
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+//可以直接导入组件
+import HomeView from "../views/HomeView.vue";
+// 定义路由
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    //也可以通过懒加载的方式
+    component: () => import("../views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  //使用history模式
+  //路由的3种模式会在以后的文章中详细讲解
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
