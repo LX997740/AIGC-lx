e97946515579d4aa10b88d37bc5e7a1db3c40fee
lx779940 <3122178991@qq.com>
2024-03-24 17:50:19 +0800

    demo-1

--- JuejinDemo/Vue-Router/Router-Demo-1/src/router/index.ts
+++ JuejinDemo/Vue-Router/Router-Demo-1/src/router/index.ts
@@ -1,19 +1,31 @@
 //用ES6解构导入
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
 //可以直接导入组件
 import HomeView from "../views/HomeView.vue";
 // 定义路由
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    //也可以通过懒加载的方式
-    component: () => import("../views/AboutView.vue"),
+    name: "Root",
+    component: () => import("../views/TheRoot.vue"),
+    //定义子路由
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        component: HomeView,
+      },
+      {
+        path: "/about",
+        name: "about",
+        //也可以通过懒加载的方式
+        component: () => import("../views/AboutView.vue"),
+      },
+    ],
   },
 ];
 
